5df6852d51c2a407ceb317659350a7ded0efa654
juanmirivas8 <juanmirivas8@gmail.com>
2022-11-03 21:58:49 +0100

    Login Persistente Terminado

--- src/app/services/login.service.ts
+++ src/app/services/login.service.ts
@@ -12,34 +12,45 @@ export class LoginService {
   loggedIn!: boolean;
   originalPath!:string;
 
-  constructor(private authService: SocialAuthService,
-    private router:Router) {
-
-    this.authService.authState.subscribe((user) => {
-      this.user = user;
-      this.loggedIn = (user != null);
-      if(this.loggedIn){
+  constructor(private authService: SocialAuthService, private router:Router) {
+      this.user = localStorage.getItem('user') ? JSON.parse(localStorage.getItem('user')!) : null;
+      if(this.user){
+        this.loggedIn=true;
         if(this.originalPath){
           this.router.navigate([this.originalPath]);
           this.originalPath='';
         }else
           this.router.navigate(['']);
       }else{
-        this.router.navigate(['/login']);
+        this.authService.authState.subscribe((user) => {
+          localStorage.setItem('user', JSON.stringify(user));
+          this.user = user;
+          this.loggedIn = (user != null);
+          if(this.loggedIn){
+            if(this.originalPath){
+              this.router.navigate([this.originalPath]);
+              this.originalPath='';
+            }else
+              this.router.navigate(['']);
+          }else{
+            this.router.navigate(['/login']);
+          }
+        });
       }
-    });
    }
   isAuth():boolean{
     return this.loggedIn;
   }
   async refreshToken(): Promise<void> {
     return this.authService.refreshAuthToken(GoogleLoginProvider.PROVIDER_ID);
+
   }
   /*
   async signInWithGoogle():Promise<SocialUser> {
     return this.authService.signIn(GoogleLoginProvider.PROVIDER_ID);
   }*/
   async signOut(): Promise<void> {
+    localStorage.removeItem('user');
     return await this.authService.signOut();
   }
 
